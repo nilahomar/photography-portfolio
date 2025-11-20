f2534bdb02954b294a875a96e080273c4a2ebc53
Nila Homar <nilahomar@mac.speedport.ip>
2025-11-20 01:28:21 +0100

    routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+//import components
+
+import Header from './components/Header';
+import AnimeRoutes from './components/AnimRoutes';
+//import Router
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//import motion
+import {motion} from 'framer-motion';
+
+
 
 const App = () => {
-  return <div>react app</div>;
+  return <div>
+    <Router>
+      <Header/>
+      <AnimeRoutes/>
+    </Router>
+  </div>;
 };
 
 export default App;
--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -1,7 +1,25 @@
 import React from 'react';
+//import pages
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Portfolio from '../pages/Portfolio';
+import Blog from '../pages/Blog';
+import Contact from '../pages/Contact';
+
+//import routes,route and useLocation hook
+
+import { Routes,Route,useLocation } from 'react-router-dom';
 
 const AnimRoutes = () => {
-  return <div>AnimRoutes</div>;
+  return <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/portfolio' element={<Portfolio/>}/>
+    <Route path='/blog' element={<Blog/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route/>
+  </Routes>
 };
 
 export default AnimRoutes;
