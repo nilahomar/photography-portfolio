6d1115c591f83dd1bd9ac8866b179b625e2d6dbf
Nila Homar <nilahomar@mac.speedport.ip>
2025-11-24 20:57:48 +0100

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Socials from './Socials';
+import Logo from '../img/header/logo.svg';
+import MobileNav from './MobileNav';
+
+//import link
+
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <Header className='bg-pink-200 fixed w-full px-[30px] lg:px[100px] z-30 h-[100px] lg:h[140px flex items-center]'>
+
+  </Header>
 };
 
 export default Header;
